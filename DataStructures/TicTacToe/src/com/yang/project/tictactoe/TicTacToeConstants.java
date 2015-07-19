035f963cf6d9ef060867cda3e8cf4657c0d95a7e
yangyuanke <yangyuanke35@gmail.com>
2015-07-20 02:14:55 +0800

    TicTacToe
    公共变量接口定义

---
+++ DataStructures/TicTacToe/src/com/yang/project/tictactoe/TicTacToeConstants.java
@@ -0,0 +1,16 @@
+package com.yang.project.tictactoe;
+/**
+ * @author yangyuanke
+ * @Description:公共常量定义
+ */
+public interface TicTacToeConstants {
+	public static int PLAER1=1;
+	public static int PLAER2=2;
+	//游戏状态
+	public static int PLAER1_WON=1;
+	public static int PLAER2_WON=2;
+	//棋盘满了，游戏结束
+	public static int END=3;
+	//游戏继续
+	public static int CONTINUE=4;
+}
